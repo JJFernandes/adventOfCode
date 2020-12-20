dfcd677871a7d3868178fb5fb5b656a95cd31ca6
JJFernandes <jjfernandes1984@outlook.com>
2020-12-19 17:45:14 -0800

    Updated code with challenge context

--- 4/4-1.py
+++ 4/4-1.py
@@ -1,6 +1,14 @@
 """
-Count the number of valid passports - those that have all required fields. 
-Treat cid as optional.
+Count the number of valid passports - those that have all required fields.
+
+byr (Birth Year)
+iyr (Issue Year)
+eyr (Expiration Year)
+hgt (Height)
+hcl (Hair Color)
+ecl (Eye Color)
+pid (Passport ID)
+cid (Country ID) - cid (Country ID) - ignored, missing or not.
 """
 
 import re
